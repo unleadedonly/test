bffefff341bdce6129c7d1340a30bf6983da606a
unleadedonly <61168426+unleadedonly@users.noreply.github.com>
2020-02-17 17:04:11 -0800

    Add files via upload

---
+++ bugs.js
@@ -0,0 +1,22 @@
+var firstName = "Robert";
+var nickName = "Bob";
+var petName = "Fluffy";
+var petType = "Turtle";
+if(firstName == "Robert"){
+    console.log("Hi Robert");
+}
+else if(firstName == "Bob"){
+    console("Hi Bob");
+}
+else{
+    console.log("Hello Stranger");
+}
+if (petType == "Dog") {       
+    console.log("Time to play fetch");
+}
+else if(petType == "Cat"){
+    console.log("Time to play with a laser pointer");
+}
+else{
+    console.log("Time to play with my pet");
+}
